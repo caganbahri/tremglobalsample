4b11a65c6244ae75c1ffc5d6d6432391b6eacc9c
caganbahri <58372822+caganbahri@users.noreply.github.com>
2022-05-11 15:49:05 +0300

    Add files via upload

---
+++ kayitdefteri.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: localhost:3306
+-- Üretim Zamanı: 11 May 2022, 15:30:43
+-- Sunucu sürümü: 10.3.30-MariaDB
+-- PHP Sürümü: 7.4.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `admin_test1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `kayitdefteri`
+--
+
+CREATE TABLE `kayitdefteri` (
+  `id` int(11) NOT NULL,
+  `adsoyad` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
+  `adres` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
+  `telefon` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
+  `eposta` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `kayitdefteri`
+--
+
+INSERT INTO `kayitdefteri` (`id`, `adsoyad`, `adres`, `telefon`, `eposta`) VALUES
+(6, 'Çağan Bahri', 'Gaziantep şehitkamil', '05535855555', 'caganbahri@gmail.com'),
+(7, 'Çağan', 'Gaziantep beykent', '05535855552', 'bahri@gmail.com');
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `kayitdefteri`
+--
+ALTER TABLE `kayitdefteri`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `kayitdefteri`
+--
+ALTER TABLE `kayitdefteri`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
